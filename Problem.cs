569d565f767111dd8aa51b5d3e78dd78a054f7f6
Muhammad Xodjayev <nofree1818@gmail.com>
2024-03-11 19:43:37 +0500

    Add project files.

---
+++ Problem.cs
@@ -0,0 +1,19 @@
+﻿namespace FizzBuzz
+{
+    public class Problem : Logic // Inheriting the Logic class because there is a method - LogicForFizzBuzz
+    {
+        public string[] FooFizzBuzz(int num) // Receiving 30
+        {
+            string[] stringArray = new string[0]; // Declaring a new array for returning the final result
+
+            for (int i = 1; i <= num; i++) // Iterating till 30 (30 is included)
+            {
+                Array.Resize(ref stringArray, stringArray.Length + 1); // Expanding the size of array in order to add the result
+                stringArray[stringArray.Length - 1] = LogicForFizzBuzz(i); // Appending the result, for getting the result sending 
+                // i to the LogicForFizzBuzz in order to check whether the number is divisible by any divisors in Program.cs
+            }
+
+            return stringArray; // Returning the result [1, foo, fizz, foo, buzz, .........]
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using FizzBuzz;
+
+class Program
+{
+    public static int[] divisors = [2, 3, 5, 7, 11, 13]; // The initial value of divisors, if the user wants he/she can change it
+    // as he/she wants
+
+    static void Main(string[] args)
+    {
+        Problem myObject = new Problem(); // creating a new object for the problem FizzBuzz
+
+        var result = myObject.FooFizzBuzz(30); // Giving 30 to the FooFizzBuzz function and receiving the array string to the result
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item); // Printing the result for checking the result
+        }
+    }
+}
